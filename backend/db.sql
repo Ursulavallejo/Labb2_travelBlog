6ea21babef18e42660aaf1cd78c033e058aa7e73
Ursula Vallejo Janne <ursulavallejo@gmail.com>
2024-11-01 07:27:07 +0100

    SQL file with comments tables to create

---
+++ backend/db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE user >> ID, name, email, password
+
+CREATE TABLE blog  >>> land_name , image_blog ,text_blog, FK_user, FK_comment?, date, author
+
+CREATE TABLE comments >> Text_comment, FK_user , FK_blog, date, author

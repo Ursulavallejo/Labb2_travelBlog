42c27fb184dd5a71f44190620e1a7ad06f1821d1
Pedram <pedram.hejazi.kenari@iths.se>
2024-11-04 15:18:24 +0100

    added table users with insert into

--- backend/db.sql
+++ backend/db.sql
@@ -7,12 +7,11 @@ CREATE TABLE users (
     pass_word VARCHAR(100) NOT NULL
 )
 
-
-
-
-
-
-
+INSERT INTO users (first_name, last_name, username, email, pass_word)
+VALUES ('Pedram', 'Hejazi Kenari', 'PHK', 'PHK@ITHS.com', 'hemligt123');
+('John', 'Doe', 'JD', 'john.doe@example.com', 'password123'),
+('Jane', 'Smith', 'JS', 'jane.smith@example.com', 'password456'),
+('Alex', 'Writer', 'AW', 'alex.writer@example.com', 'password789');
 
 
 
@@ -20,17 +19,27 @@ CREATE TABLE blogs (
     blog_id serial PRIMARY KEY,
     land_name VARCHAR(100) NOT NULL,
     image_blog TEXT,
+    title_blog VARCHAR(100) NOT NULL,
     text_blog TEXT NOT NULL,
-    FK_user INT REFERENCES "user" (id) ON DELETE CASCADE,
+    FK_users INT REFERENCES users(user_id) ON DELETE CASCADE,
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     author VARCHAR(50)
 );
 
+INSERT INTO blogs (land_name, image_blog, title_blog, text_blog, FK_users, author)
+VALUES
+('Sverige', 'https://example.com/sverige.jpg', 'Sverige Reseguide', 'En guide till Sveriges bästa resmål.', 1, 'John Doe'),
+('Italien', 'https://example.com/italien.jpg', 'Utforska italienska maträtter.',  'Mat i Italien', 2, 'Jane Smith'),
+('Norge', 'https://example.com/norge.jpg', 'Norges bästa naturupplevelser.', 'Naturupplevelser i Norge', 3, 'Alex Writer');
+
+
+
+
 CREATE TABLE comments (
     comment_id SERIAL PRIMARY KEY,
     text_comment TEXT NOT NULL,
-    FK_user INT REFERENCES users(id),
-    FK_blog INT REFERENCES blogs(id),
+    FK_users INT REFERENCES users(user_id),
+    FK_blogs INT REFERENCES blogs(blog_id),
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     username VARCHAR(50) NOT NULL
 );
@@ -40,15 +49,3 @@ VALUES
 ('Fantastisk artikel!', 1, 1, 'john_doe'),
 ('Väldigt hjälpsamt, tack!', 2, 2, 'jane_smith'),
 ('Kan du ge fler exempel?', 3, 3, 'alex_writer');
-
-
-
-
-
-
-
-
-
-
-
-CREATE TABLE comments >>ID_PK, Text_comment, FK_userID , FK_blog, date,

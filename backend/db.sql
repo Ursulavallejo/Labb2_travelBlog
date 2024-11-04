7329012d19115992a8d8bb939e77ec68858edadb
Gzd Akgn <gozzde0990@gmail.com>
2024-11-04 14:52:44 +0100

    "Added comments table to database, updated backend to use PostgreSQL, and installed required dependencies."

--- backend/db.sql
+++ backend/db.sql
@@ -16,12 +16,25 @@ CREATE TABLE blogs (
     land_name VARCHAR(100) NOT NULL,
     image_blog TEXT,
     text_blog TEXT NOT NULL,
-    fk_user INT REFERENCES "user" (id) ON DELETE CASCADE,
+    FK_user INT REFERENCES "user" (id) ON DELETE CASCADE,
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     author VARCHAR(50)
 );
 
+CREATE TABLE comments (
+    comment_id SERIAL PRIMARY KEY,
+    text_comment TEXT NOT NULL,
+    FK_user INTEGER REFERENCES users(id),
+    FK_blog INTEGER REFERENCES blogs(id),
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    username VARCHAR(50) NOT NULL
+);
 
+INSERT INTO comments (Text_comment, FK_user, FK_blog, username)
+VALUES
+('Fantastisk artikel!', 1, 1, 'john_doe'),
+('Väldigt hjälpsamt, tack!', 2, 2, 'jane_smith'),
+('Kan du ge fler exempel?', 3, 3, 'alex_writer');
 
 
 

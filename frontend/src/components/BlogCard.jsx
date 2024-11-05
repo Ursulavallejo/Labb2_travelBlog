fd8977f0dd6a3debd9040704bd898515c5531f7d
Gzd Akgn <gozzde0990@gmail.com>
2024-11-05 10:11:34 +0100

    "Removed useState and conditional rendering of CommentModal, now always rendering CommentModal"

--- frontend/src/components/BlogCard.jsx
+++ frontend/src/components/BlogCard.jsx
@@ -1,18 +1,10 @@
-import { useState } from "react";
 import CommentModal from "./CommentModal";
 export default function BlogCard() {
-  const [showCommentModal, setShowCommentModal] = useState(false);
-  // Modal bootstrap kommer hit
   return (
     <div>
       <h3>En blog om ett land</h3>
       <p>Det här är en kort beskrivning av bloggen.</p>
-      <button
-        className='btn btn-primary'
-        onClick={() => setShowCommentModal(!showCommentModal)}>
-        {showCommentModal ? "Dölj Kommentarer" : "Visa Kommentarer"}
-      </button>
-      {showCommentModal && <CommentModal />}
+      <CommentModal />
     </div>
   );
 }

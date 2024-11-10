b4c5fe3e9d013e6c4214cc38683e64d430c9a787
Ursula Vallejo Janne <ursulavallejo@gmail.com>
2024-11-10 12:49:48 +0100

    update layout to reactboostrat profile

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../Context/UserContext';
+import { Button, Spinner, Form, Container } from 'react-bootstrap';
 
 export default function Profile() {
   const { ID } = useContext(UserContext);
@@ -40,114 +41,90 @@ export default function Profile() {
   return (
     <>
       {loading ? (
-        <div
-          style={{ width: '5rem', height: '5rem' }}
-          className="spinner-border mx-auto my-auto"
+        <Spinner
+          animation="border"
           role="status"
+          style={{ width: '5rem', height: '5rem' }}
+          className="mx-auto my-auto"
         />
       ) : (
-        <div className="d-flex flex-column justify-content-center align-items-center my-auto">
+        <Container className="d-flex flex-column justify-content-center align-items-center my-auto">
           <h2 style={{ color: 'white' }}>Ändra dina uppgifter</h2>
-
           <div
             id="containerReg"
-            className="profile-container rounded-5 p-5"
+            className="rounded-5 p-5"
             style={{ backgroundColor: '#123456' }}
           >
-            <form id="register" className="d-flex flex-column mx-auto">
-              <label
-                htmlFor="fname"
-                className="mt-3"
-                style={{ color: 'white' }}
-              >
-                Ange förnamn:
-              </label>
-              <input
-                id="fname"
-                name="fname"
-                type="text"
-                placeholder="Förnamn..."
-                defaultValue={profile.fname}
-                disabled
-                style={{ backgroundColor: 'white', color: '#123456' }}
-              />
-              <label
-                htmlFor="lname"
-                className="mt-3"
-                style={{ color: 'white' }}
-              >
-                Ange efternamn:
-              </label>
-              <input
-                id="lname"
-                name="lname"
-                type="text"
-                placeholder="Efternamn..."
-                defaultValue={profile.lname}
-                disabled
-                style={{ backgroundColor: 'white', color: '#123456' }}
-              />
-              <label
-                htmlFor="username"
-                className="mt-3"
-                style={{ color: 'white' }}
-              >
-                Ange användarenamn:
-              </label>
-              <input
-                id="username"
-                name="username"
-                type="text"
-                placeholder="Användarnamn..."
-                defaultValue={profile.username}
-                disabled
-                style={{ backgroundColor: 'white', color: '#123456' }}
-              />
-              <label
-                htmlFor="emailReg"
-                className="mt-3"
-                style={{ color: 'white' }}
-              >
-                Ange email:
-              </label>
-              <input
-                id="emailReg"
-                name="emailReg"
-                type="text"
-                placeholder="Email..."
-                defaultValue={profile.email}
-                disabled
-                style={{ backgroundColor: 'white', color: '#123456' }}
-              />
+            <Form className="d-flex flex-column mx-auto">
+              <Form.Group className="mt-3">
+                <Form.Label style={{ color: 'white' }}>
+                  Ange förnamn:
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Förnamn..."
+                  defaultValue={profile.fname}
+                  disabled
+                  style={{ backgroundColor: 'white', color: '#123456' }}
+                />
+              </Form.Group>
+
+              <Form.Group className="mt-3">
+                <Form.Label style={{ color: 'white' }}>
+                  Ange efternamn:
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Efternamn..."
+                  defaultValue={profile.lname}
+                  disabled
+                  style={{ backgroundColor: 'white', color: '#123456' }}
+                />
+              </Form.Group>
+
+              <Form.Group className="mt-3">
+                <Form.Label style={{ color: 'white' }}>
+                  Ange användarenamn:
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Användarnamn..."
+                  defaultValue={profile.username}
+                  disabled
+                  style={{ backgroundColor: 'white', color: '#123456' }}
+                />
+              </Form.Group>
+
+              <Form.Group className="mt-3">
+                <Form.Label style={{ color: 'white' }}>Ange email:</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Email..."
+                  defaultValue={profile.email}
+                  disabled
+                  style={{ backgroundColor: 'white', color: '#123456' }}
+                />
+              </Form.Group>
 
               <div className="d-flex justify-content-center mt-4">
-                <button
-                  type="button"
+                <Button
+                  variant="warning"
+                  className="w-50 mx-2"
                   onClick={handleConfirm}
-                  className="w-50 mt-3 rounded-2 blue"
-                  style={{
-                    backgroundColor: '#e27e0a',
-                    color: 'white',
-                    border: 'none',
-                  }}
                 >
                   Bekräfta uppdatering
-                </button>
-                <button
-                  type="button"
+                </Button>
+                <Button
+                  variant="outline-warning"
+                  className="w-50 mx-2"
                   onClick={handleCancel}
-                  className="w-50 mt-3 rounded-2 blue ms-3"
-                  style={{
-                    color: '#e27e0a',
-                    border: '1px solid #e27e0a',
-                  }}
                 >
                   Avbryt
-                </button>
+                </Button>
               </div>
-            </form>
+            </Form>
           </div>
-        </div>
+        </Container>
       )}
     </>
   );
